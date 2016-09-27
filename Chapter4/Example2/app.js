2f2109d832502a3f2f9c0c4d4b90ce733d49444d
Deshawn Dana <dashwen@gmail.com>
2016-09-26 18:04:02 -0700

    fully functional simple app chat with two forms utilizing one chat box

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,25 +1,52 @@
 var main = function () {
     "use strict";
 
-    var addCommentFromInputBox = function () {
+    var buttonClicked1 = false;
+    var buttonClicked2 = false;
+
+    var addCommentFromInputBox = function (buttonClicked2) {
         var $new_comment;
 
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
+        if ($(".comment-input1 input").val() !== "" && buttonClicked1 == true) {
+            $new_comment = $("<p id='User1'>").text($(".comment-input1 input").val());
             $new_comment.hide();
             $(".comments").append($new_comment);
             $new_comment.fadeIn();
-            $(".comment-input input").val("");
+            $(".comment-input1 input").val("");
+            buttonClicked1 = false;
+        }
+
+        else if($(".comment-input2 input").val() !== "" && buttonClicked2 == true) {
+            $new_comment = $("<p id='User2'>").text($(".comment-input2 input").val());
+            $new_comment.hide();
+            $(".comments").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-input2 input").val("");
+            buttonClicked2 = false;
         }
     };
 
-    $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
+    $(".comment-input1 button").on("click", function (event) {
+        buttonClicked1 = true;
+        addCommentFromInputBox(buttonClicked1);
     });
 
-    $(".comment-input input").on("keypress", function (event) {
+    $(".comment-input1 input").on("keypress", function (event) {
         if (event.keyCode === 13) {
-            addCommentFromInputBox();
+            buttonClicked1 = true;
+            addCommentFromInputBox(buttonClicked1);
+        }
+    });
+
+    $(".comment-input2 button").on("click", function (event) {
+        buttonClicked2 = true;
+        addCommentFromInputBox(buttonClicked2);
+    });
+
+    $(".comment-input2 input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            buttonClicked2 = true;
+            addCommentFromInputBox(buttonClicked2);
         }
     });
 };
