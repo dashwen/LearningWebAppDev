9b7e86ff8595b2721be3bd1c6e7f61dd411dea38
Deshawn Dana <dashwen@gmail.com>
2016-09-27 18:08:36 -0700

    Fixed validator bugs, final version

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,52 +1,53 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
+
+
 var main = function () {
-    "use strict";
+    'use strict';
 
     var buttonClicked1 = false;
-    var buttonClicked2 = false;
 
-    var addCommentFromInputBox = function (buttonClicked2) {
-        var $new_comment;
+    var addCommentFromInputBox = function (buttonClicked1) {
+        var $newComment;
 
-        if ($(".comment-input1 input").val() !== "" && buttonClicked1 === true) {
-            $new_comment = $("<p id='User1'>").text($(".comment-input1 input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input1 input").val("");
+        if ($('.comment-input1 input').val() !== '' && buttonClicked1 === true) {
+            $newComment = $("<p class='User1'>").text($('.comment-input1 input').val());
+            $newComment.hide();
+            $('.comments').append($newComment);
+            $newComment.fadeIn();
+            $('.comment-input1 input').val('');
             buttonClicked1 = false;
         }
 
-        else if($(".comment-input2 input").val() !== "" && buttonClicked2 === true) {
-            $new_comment = $("<p id='User2'>").text($(".comment-input2 input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input2 input").val("");
-            buttonClicked2 = false;
+        else if($('.comment-input2 input').val() !== '' && buttonClicked1 === false) {
+            $newComment = $("<p class='User2'>").text($('.comment-input2 input').val());
+            $newComment.hide();
+            $('.comments').append($newComment);
+            $newComment.fadeIn();
+            $('.comment-input2 input').val('');
         }
     };
 
-    $(".comment-input1 button").on("click", function (event) {
+    $('.comment-input1 button').on('click', function (event) {
         buttonClicked1 = true;
         addCommentFromInputBox(buttonClicked1);
     });
 
-    $(".comment-input1 input").on("keypress", function (event) {
+    $('.comment-input1 input').on('keypress', function (event) {
         if (event.keyCode === 13) {
             buttonClicked1 = true;
             addCommentFromInputBox(buttonClicked1);
         }
     });
 
-    $(".comment-input2 button").on("click", function (event) {
-        buttonClicked2 = true;
-        addCommentFromInputBox(buttonClicked2);
+    $('.comment-input2 button').on('click', function (event) {
+        buttonClicked1 = false;
+        addCommentFromInputBox(buttonClicked1);
     });
 
-    $(".comment-input2 input").on("keypress", function (event) {
+    $('.comment-input2 input').on('keypress', function (event) {
         if (event.keyCode === 13) {
-            buttonClicked2 = true;
-            addCommentFromInputBox(buttonClicked2);
+            buttonClicked1 = false;
+            addCommentFromInputBox(buttonClicked1);
         }
     });
 };
